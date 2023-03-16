5cc839bae7c84453345a03b16acfd6dd5bbc68af
nynameiskayky <kaykyvieiraa@hotmail.com>
2023-03-16 16:28:17 -0300

    Create first Route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,15 @@
-console.log('rodando')
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+/*
+  Método HTTP: Get, Post, Put, Patch, Delete
+*/
+
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app.listen({
+  port: 3333
+})
