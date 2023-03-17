9feda101da0230bb95afde6e71fdbe4e85535b65
nynameiskayky <kaykyvieiraa@hotmail.com>
2023-03-16 21:44:57 -0300

    config prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,23 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-/*
-  Método HTTP: Get, Post, Put, Patch, Delete
-*/
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "L"
+      }
+    }
+  })
 
-app.get('/', () => {
-  return 'Hello World'
+  return habits
 })
 
 app.listen({
   port: 3333
+}).then(() => {
+  console.log('HTTP Server Runnning!')
 })
